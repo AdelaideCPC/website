049cf16eda3a8cc731010417d23bcca56054553b
Ray Okamoto <git@rayokamoto.com>
2026-01-22 14:58:05 +1030

    feat: content delivery optimisations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: "auto",
+  },
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssMinify: "lightningcss",
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+        },
+      },
+    },
   },
 });
