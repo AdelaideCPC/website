8317f54f7c30d2bec4fc66c3266d6aa9df336247
Ray Okamoto <git@rayokamoto.com>
2026-01-22 15:23:06 +1030

    feat: add links to discord, instagram in paragraphs

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,2 +1,10 @@
 export const CONTACT_EMAIL = "contact@acpc.io";
 export const JOIN_LINK = "https://forms.gle/KNQsRjctczBmYrg86";
+
+// Social Links
+export const DISCORD_LINK = "https://discord.gg/9yBJs68Sj4";
+export const FACEBOOK_LINK = "https://www.facebook.com/groups/uoacpc";
+export const GITHUB_LINK = "https://github.com/AdelaideCPC/";
+export const INSTAGRAM_LINK = "https://www.instagram.com/adelaidecpc";
+export const LINKEDIN_LINK =
+  "https://www.linkedin.com/company/competitive-programming-club-of-the-university-of-adelaide";
