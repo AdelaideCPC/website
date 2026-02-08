26944aee6d07a32966e2270a4d469fa7de3f7316
Ray Okamoto <git@rayokamoto.com>
2026-02-08 18:56:02 +1030

    chore: update linkedin link

--- src/data/links.ts
+++ src/data/links.ts
@@ -6,6 +6,5 @@ export const DISCORD_LINK = "https://discord.gg/9yBJs68Sj4";
 export const FACEBOOK_LINK = "https://www.facebook.com/groups/uoacpc";
 export const GITHUB_LINK = "https://github.com/AdelaideCPC/";
 export const INSTAGRAM_LINK = "https://www.instagram.com/adelaidecpc";
-export const LINKEDIN_LINK =
-  "https://www.linkedin.com/company/competitive-programming-club-of-the-university-of-adelaide";
+export const LINKEDIN_LINK = "https://www.linkedin.com/company/adelaide-cpc";
 export const YOUTUBE_LINK = "https://www.youtube.com/@adelaidecpc";
